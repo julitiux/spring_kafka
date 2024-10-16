77f9a0ba36ba1766d16bffe70a69eb91992e7172
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-15 23:37:17 -0600

    add bean KafkaTemplate

--- src/main/java/com/spring_kafka/producer/KafkaProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaProducer.java
@@ -1,9 +1,13 @@
 package com.spring_kafka.producer;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaProducer {
+
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
 }
