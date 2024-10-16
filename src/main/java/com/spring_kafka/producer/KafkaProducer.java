6775a3ff8f582f6a758ec970601a2b8a1a66dfe7
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-15 23:34:09 -0600

    add tags like Service from Spring and RequiredArgsConstructor from Lombok

--- src/main/java/com/spring_kafka/producer/KafkaProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaProducer.java
@@ -1,4 +1,9 @@
 package com.spring_kafka.producer;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class KafkaProducer {
 }
