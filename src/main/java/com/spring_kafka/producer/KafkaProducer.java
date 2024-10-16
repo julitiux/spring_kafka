b7a152bbd3ec001f905296b797409b5793578d29
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-15 23:42:38 -0600

    print log with Slf4j

--- src/main/java/com/spring_kafka/producer/KafkaProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaProducer.java
@@ -1,16 +1,20 @@
 package com.spring_kafka.producer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducer {
 
   private final KafkaTemplate<String, String> kafkaTemplate;
 
   public void sendMessage(String message) {
+
+    log.info("Sending message to quickstart-events Topic:: {}", message);
     kafkaTemplate.send("quickstart-events", message);
   }
 
