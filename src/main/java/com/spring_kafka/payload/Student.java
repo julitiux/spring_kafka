8f0c28d08ad2378e66812d41d52f4a8538907b20
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 17:53:32 -0600

    add proeprties into payload Studen

--- src/main/java/com/spring_kafka/payload/Student.java
+++ src/main/java/com/spring_kafka/payload/Student.java
@@ -1,4 +1,14 @@
 package com.spring_kafka.payload;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
+
+  private int id;
+  private String firstName;
+  private String lastName;
+
 }
