7372c1918d95ee98ab48c79fc90be07c1eb41600
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 14:18:40 -0600

    add tag KafkaListener

--- src/main/java/com/spring_kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/spring_kafka/consumer/KafkaConsumer.java
@@ -1,13 +1,17 @@
 package com.spring_kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class KafkaConsumer {
 
+  @KafkaListener
   public void consumerMessage(String message) {
+
+    log.info("Consuming the message from quickstart-events Topic:: {}", message);
   }
 
 }
