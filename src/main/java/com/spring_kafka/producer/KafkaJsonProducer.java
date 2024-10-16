3c96302338ed1a317c597858ff40dfb32767cb20
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 17:55:51 -0600

    inject KafkaTemplate<String, Student>, String cos it will be a String key and Student cos it will be a value Json

--- src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
@@ -1,9 +1,14 @@
 package com.spring_kafka.producer;
 
+import com.spring_kafka.payload.Student;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaJsonProducer {
+
+  private final KafkaTemplate<String, Student> kafkaTemplate;
+
 }
