d7aedec043523c2bc22701ac94643eedd2e9107a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-15 23:31:39 -0600

    implement the consumer Topic with a TopicBuilder

--- src/main/java/com/spring_kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/spring_kafka/config/KafkaTopicConfig.java
@@ -1,7 +1,18 @@
 package com.spring_kafka.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+
+  @Bean
+  public NewTopic quickstartEvents() {
+    return TopicBuilder
+      .name("quickstart-events") // name of Topic
+      .build();
+  }
+
 }
