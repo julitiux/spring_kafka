4921983a44c8aebe62c22c6e147ef898265a2891
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 17:51:10 -0600

    add RequiredArgsConstructor

--- src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
@@ -1,7 +1,9 @@
 package com.spring_kafka.producer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaJsonProducer {
 }
