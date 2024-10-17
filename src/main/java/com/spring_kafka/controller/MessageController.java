1d8182f82e33b1f21d178ef5dd8c75d63185df45
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 18:24:21 -0600

    inject the bean KafkaJsonProducer

--- src/main/java/com/spring_kafka/controller/MessageController.java
+++ src/main/java/com/spring_kafka/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.spring_kafka.controller;
 
+import com.spring_kafka.producer.KafkaJsonProducer;
 import com.spring_kafka.producer.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
   private final KafkaProducer kafkaProducer;
+  private final KafkaJsonProducer kafkaJsonProducer;
 
   @PostMapping
   public ResponseEntity<String> sendMessage(@RequestBody String message) {
