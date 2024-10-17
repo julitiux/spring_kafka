04ff335ed2bb80fcfdf6e29323ed520b2537f314
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 18:01:45 -0600

    implement a Message with type Student Payload

--- src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
@@ -3,6 +3,9 @@ package com.spring_kafka.producer;
 import com.spring_kafka.payload.Student;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +13,14 @@ import org.springframework.stereotype.Service;
 public class KafkaJsonProducer {
 
   private final KafkaTemplate<String, Student> kafkaTemplate;
+
+  public void sendMessage(Student student){
+
+    Message<Student> message = MessageBuilder
+      .withPayload(student)
+      .setHeader(KafkaHeaders.TOPIC, "quickstart-events")
+      .build();
+
+  }
 
 }
