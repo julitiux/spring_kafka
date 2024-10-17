0212fde41b096cf787a5d390e94e2e7ae77eeeab
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 22:42:52 -0600

    implement method consumerJsonMessage but in the case using the payload Student

--- src/main/java/com/spring_kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/spring_kafka/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.spring_kafka.consumer;
 
+import com.spring_kafka.payload.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,10 +9,17 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaConsumer {
 
-  @KafkaListener(topics = "quickstart-events", groupId = "myGroup")
+//  @KafkaListener(topics = "quickstart-events", groupId = "myGroup")
   public void consumerMessage(String message) {
 
     log.info("Consuming the message from quickstart-events Topic:: {}", message);
+  }
+
+
+  @KafkaListener(topics = "quickstart-events", groupId = "myGroup")
+  public void consumerJsonMessage(Student student) {
+
+    log.info("Consuming the message from quickstart-events Topic:: {}", student.toString());
   }
 
 }
