e28ced4b906b223ddf60fcb17d055da4da4c4534
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-10-16 22:44:40 -0600

    add ToString lombok

--- src/main/java/com/spring_kafka/payload/Student.java
+++ src/main/java/com/spring_kafka/payload/Student.java
@@ -2,9 +2,11 @@ package com.spring_kafka.payload;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Student {
 
   private int id;
