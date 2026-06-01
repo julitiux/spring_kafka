729ba43b24d284fa37fa097b48b1389164e4da13
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-05-31 22:40:12 -0600

    implement method quickStartEvents with a return object newTopic

--- src/main/java/com/spring_kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/spring_kafka/config/KafkaTopicConfig.java
@@ -1,4 +1,17 @@
 package com.spring_kafka.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaTopicConfig {
+
+  @Bean
+  public NewTopic quickStartEvents() {
+    return TopicBuilder
+      .name("quickstart-events")
+      .build();
+  }
 }
