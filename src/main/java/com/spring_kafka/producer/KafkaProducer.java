a5414f76c0845d74fda398fe59f7e28b6e977ae3
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-01 18:28:01 -0600

    implement method to send Message Producer

--- src/main/java/com/spring_kafka/producer/KafkaProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaProducer.java
@@ -1,11 +1,23 @@
 package com.spring_kafka.producer;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import static java.lang.String.format;
+
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class KafkaProducer {
+
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendMessage(String message) {
+    log.info(format("Sending message to kafka: %s", message));
+    kafkaTemplate.send("quickstart-events", message);
+  }
 }
