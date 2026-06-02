5db6ef7279858cbffb39de3c7f9ddc7b93e129af
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-01 18:45:21 -0600

    implement MessageController just for send message with a post resquest

--- src/main/java/com/spring_kafka/web/MessageController.java
+++ src/main/java/com/spring_kafka/web/MessageController.java
@@ -1,4 +1,23 @@
 package com.spring_kafka.web;
 
+import com.spring_kafka.producer.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/messages")
 public class MessageController {
+
+  private final KafkaProducer producer;
+
+  public MessageController(KafkaProducer producer) {
+    this.producer = producer;
+  }
+
+  @PostMapping()
+  public ResponseEntity<String> sendMessage(@RequestBody String message){
+
+    producer.sendMessage(message);
+    return ResponseEntity.ok().body("Message queued successfully");
+  }
 }
