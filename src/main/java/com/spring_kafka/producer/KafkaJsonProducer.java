f21de25644956ebb0492df837090b7e1135dc560
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-06 00:29:30 -0600

    implement method sendMessage using a class Student with Json format

--- src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
@@ -2,6 +2,9 @@ package com.spring_kafka.producer;
 
 import com.spring_kafka.payload.Student;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +14,16 @@ public class KafkaJsonProducer {
 
   public KafkaJsonProducer(KafkaTemplate<String, Student> kafkaTemplate) {
     this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendMessage(Student student) {
+
+    Message<Student> message =
+      MessageBuilder
+        .withPayload(student)
+        .setHeader(KafkaHeaders.TOPIC, "quickstart-events")
+        .build();
+
+    kafkaTemplate.send(message);
   }
 }
