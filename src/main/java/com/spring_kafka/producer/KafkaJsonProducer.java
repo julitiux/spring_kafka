2b7a39f287009b7506c0f6858f5fd311ffb233ce
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-06 00:20:31 -0600

    add bean KafkaTemplate

--- src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
+++ src/main/java/com/spring_kafka/producer/KafkaJsonProducer.java
@@ -1,4 +1,15 @@
 package com.spring_kafka.producer;
 
+import com.spring_kafka.payload.Student;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaJsonProducer {
+
+  private final KafkaTemplate<String, Student> kafkaTemplate;
+
+  public KafkaJsonProducer(KafkaTemplate<String, Student> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
 }
