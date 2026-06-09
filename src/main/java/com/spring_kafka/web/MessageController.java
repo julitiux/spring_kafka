493cf98670e867fcdfa063f955d5b04bc0c903a0
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-09 00:03:29 -0600

    rename the bean producer to kafkaProducer

--- src/main/java/com/spring_kafka/web/MessageController.java
+++ src/main/java/com/spring_kafka/web/MessageController.java
@@ -2,22 +2,25 @@ package com.spring_kafka.web;
 
 import com.spring_kafka.producer.KafkaProducer;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/messages")
 public class MessageController {
 
-  private final KafkaProducer producer;
+  private final KafkaProducer kafkaProducer;
 
   public MessageController(KafkaProducer producer) {
-    this.producer = producer;
+    this.kafkaProducer = producer;
   }
 
   @PostMapping()
-  public ResponseEntity<String> sendMessage(@RequestBody String message){
+  public ResponseEntity<String> sendMessage(@RequestBody String message) {
 
-    producer.sendMessage(message);
+    kafkaProducer.sendMessage(message);
     return ResponseEntity.ok().body("Message queued successfully");
   }
 }
