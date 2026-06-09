de8c6692f463d9b8d10efb508ab5a05658ee366b
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-09 17:19:35 -0600

    implement method to consume a content from message Topic quickstart-events

--- src/main/java/com/spring_kafka/consumer/KafkaJsonConsumer.java
+++ src/main/java/com/spring_kafka/consumer/KafkaJsonConsumer.java
@@ -1,4 +1,19 @@
 package com.spring_kafka.consumer;
 
+import com.spring_kafka.payload.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class KafkaJsonConsumer {
+
+  @KafkaListener(topics = "quickstart-events", groupId = "myGroup")
+  public void consumeJsonMessage(Student student) {
+
+    log.info(String.format("Consuming the message from quickstart-events Topi :: %s", student));
+  }
+
+
 }
